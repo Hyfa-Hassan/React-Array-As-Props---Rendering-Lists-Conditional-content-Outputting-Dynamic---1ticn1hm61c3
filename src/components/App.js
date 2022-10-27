2e051ae8141e14a29a1fab2d83b343dcc27a878a
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-27 12:36:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ const Join = (props) =>{
   return(
     <div id ="join">
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
-      
+      {arr.map((item)=>
+         <p>{item.props.words.join(",")}</p>
+       )}
     </div>
   )
 }
